3794e36b1ffdda08a2104e66dceecb0b76de2db8
Sunyan Gu <1138426807@qq.com>
2016-12-06 09:00:14 +0800

    上传测试

--- w2c_nbow/w2c_nbow.py
+++ w2c_nbow/w2c_nbow.py
@@ -287,6 +287,7 @@ class Document_distance(object):
             for sent in j:
                 sentence =  sent.strip().split()
                 text_sentence.append(sentence)
+            break
         #     document.append(text)
         # doc = []
         # doc.append(document[0])
@@ -297,8 +298,8 @@ class Document_distance(object):
         tfidf = models.TfidfModel(corpus)  # 统计tfidf
         print "lda"
         corpus_tfidf = tfidf[corpus]  # 得到每个文本的tfidf向量，稀疏矩阵
-        lda = models.LdaModel(corpus_tfidf, id2word=dic, num_topics=25)
-        for i in range(25):
+        lda = models.LdaModel(corpus_tfidf, id2word=dic, num_topics=2)
+        for i in range(2):
             print lda.print_topic(i)
         pass
         # corpus_lda = lda[corpus_tfidf]  # 每个文本对应的LDA向量，稀疏的，元素值是隶属与对应序数类的权重
